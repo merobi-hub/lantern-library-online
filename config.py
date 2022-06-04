483bd6f5a248d240a5174e7c0b2dcf34f19b5ae9
Michael Robinson <merobi@gmail.com>
2022-06-04 16:06:49 -0400

    add API key def to config
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess...'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False #turns off notifications from db
+    API_KEY = os.environ.get('API_KEY')
