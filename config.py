019c1e256fa9b9db5e68700dd2b0a3ff48fd0519
Michael Robinson <merobi@gmail.com>
2022-06-04 16:16:47 -0400

    update config.py
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- config.py
+++ config.py
@@ -7,4 +7,3 @@ class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess...'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False #turns off notifications from db
-    API_KEY = os.environ.get('API_KEY')
