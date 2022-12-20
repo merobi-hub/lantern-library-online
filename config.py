5cb5b5e39361b3070cd3d576fc4ec3cf8b49b17e
Michael Robinson <merobi@gmail.com>
2022-12-20 10:21:53 -0500

    recreate db
    Signed-off-by: Michael Robinson <merobi@gmail.com>

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config():
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess...'
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1) or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False #turns off notifications from db
