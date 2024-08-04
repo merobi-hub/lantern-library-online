a85057f66792d3970df3c5df8e8c5a9dddd5cc83
merobi-hub <merobi@gmail.com>
2024-08-04 14:41:17 -0400

    Fixed flash msg in auth.
    Signed-off-by: merobi-hub <merobi@gmail.com>

--- book_inventory/authentication/routes.py
+++ book_inventory/authentication/routes.py
@@ -1,21 +1,19 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from book_inventory.models import User, db, check_password_hash
 from book_inventory.forms import UserLoginForm
+from flask_sqlalchemy import DBAPIError 
 
 from flask_login import login_user, logout_user, current_user, login_required
 
 auth = Blueprint('auth', __name__, template_folder='auth_templates')
 
-# ROUTE WITH CODE FOR FORM
 @auth.route('/signup', methods = ['GET', 'POST'])
 def signup():
     form = UserLoginForm()
     try:
         if request.method == 'POST' and form.validate_on_submit():
-            #username = form.username.data
             email = form.email.data 
             password = form.password.data 
-            print(email, password)
 
             user = User(email, password = password)
             db.session.add(user)
@@ -38,18 +36,18 @@ def signin():
             email = form.email.data
             password = form.password.data
   
-            logged_user = ''
-            logged_user = User.query.filter(User.email == email).first()
-            
-            if logged_user == '':
-                flash('User not found. Please try again.', 'error')
+            try:
+                logged_user = User(email, password = password)
+                db.session.get(logged_user).first()
+            except DBAPIError:
+                flash('User not found. Please try again.', 'auth-failure')
                         
             elif check_password_hash(logged_user.password, password):
                 login_user(logged_user)
                 flash('You were successfully logged in.', 'auth-success')
                 return redirect(url_for('site.home'))
             
-            flash('Your password is incorrect.', 'error')
+            flash('Your password is incorrect.', 'auth-failure')
             return redirect(url_for('auth.signin'))
     except:
         raise Exception("That didn't work. Please try again.")
